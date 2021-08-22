3362aec57840ca38e9f8f1f0f21c59679a901d3f
manueloglez <mang.95@gmail.com>
2021-08-21 23:07:48 -0700

    homepage and login started

--- farmage_client/src/App.js
+++ farmage_client/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+import  {User} from './api'
+import HomepageLayout from './components/Homepage';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import SignInPage from './components/SignInPage';
+import AuthRoute from './components/AuthRoute';
+import SignUpPage from './components/SignUpPage';
+import NotFoundPage from './components/NotFoundPage';
+
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  
+  const getCurrentUser = () => {
+    return User.current().then(user => {
+      if (user.id) {
+        setUser(user);
+      }
+    });
+  }
+  
+  const onSignOut = () => {
+    setUser(null);
+  }
+  
+    useEffect(() => {
+      getCurrentUser();
+    }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomepageLayout}/>
+          <Route exact path='/sign_in' render={
+            (routeProps) => <SignInPage {...routeProps} onSignIn={getCurrentUser}/>
+          }/>
+          <Route exact path='/sign_up' render={
+            (routeProps) => <SignUpPage {...routeProps} onSignUp={getCurrentUser}/> 
+          }/>
+          <Route component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
