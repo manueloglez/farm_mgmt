418daedb9b9735ee834719e52e2501defbd3f28e
manueloglez <mang.95@gmail.com>
2021-08-22 21:44:05 -0700

    login and fields view created

--- farmage_client/src/components/AuthRoute.js
+++ farmage_client/src/components/AuthRoute.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import {Route, Redirect} from 'react-router-dom'
-
-const AuthRoute = ({isAuthenticated, component: Component, ...routeProps}) => {
-  if(isAuthenticated){
-    return <Route {...routeProps} component={Component}/>
-  } else {
-    return <Redirect to='/sign_in'/>
-  }
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+const AuthRoute=props=>{
+  const {isAuthenticated, component:Component, ...restProps}=props
+    return (
+      <Route {...restProps}
+      render={ (routeProps)=>{
+        if (isAuthenticated){
+          return <Component {...routeProps} currentUser={isAuthenticated}/>
+        }
+        else {
+          return <Redirect to="/sign_in"/>
+        }}
+    }/>
+    )
 }
-
 export default AuthRoute
